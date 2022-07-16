8b2833f318febb54a4784eaabad56f226bc1766b
Bhushnp21 <bhushnp21@gmail.com>
2022-07-16 22:08:06 +0530

    my 2nd commit from p1

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -7,6 +7,14 @@ public class LoginPageTest extends BaseTest {
 	@Test
 	public void loginPageTitleTest() {
 		System.out.println("fot git testing2");
+		System.out.println("fot git testing3");
+		System.out.println("fot git testing4");
+		System.out.println("fot git testing5");
+		System.out.println("fot git testing6");
+		System.out.println("fot git testing7");
+		//testing purpose
+		
+		
 	}
 
 }
